262ded4769cf060f1e8f61275a3024ead2cb08e0
j-west <westja89@gmail.com>
2017-03-02 15:35:43 -0600

    Add if else to check length of array after map and filter run

--- args.js
+++ args.js
@@ -1,1 +1,29 @@
+#!/usr/bin/env node
 
+//  Get arguments
+let [,, ...nums] = process.argv;
+
+//  If there are no arguments, log 0 and exit
+if (nums.length === 0) {
+  console.log(0);
+  process.exit();
+}
+
+//  Use .map to parse each index of nums to Numbers,
+//  then filter out any index that is not a number,
+//  finally, sum each index using reduce
+
+let args = nums.map((num) => parseFloat(num))
+               .filter((num) => !isNaN(num));
+
+
+//  Check if args.length is 0, happens if no numbers are entered
+
+if (args.length === 0) {
+  console.log(`Please enter numbers`);
+  process.exit();
+} else {
+    args = args.reduce((sum, num) => sum += num)
+    //  Log sum to console
+    console.log(args);
+  }
